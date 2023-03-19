523c1f625489d2cfc239ff47cb5a834265cc1abc
Manav <chavdamanav17@gmail.com>
2023-03-19 16:40:59 +0530

    Chnage UI With Global variable & New Window Feature Added

--- jiraBoard/src/app/components/dashboard/header/header.component.ts
+++ jiraBoard/src/app/components/dashboard/header/header.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Inject, OnInit } from '@angular/core';
 import { CommonService } from '../service/common.service';
 import { environment } from 'src/environments/environment';
 import { PrimeModalService } from '../prime-modal/prime-modal.service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -14,7 +15,10 @@ export class HeaderComponent implements OnInit {
   arrowImg:any;
   isModalOpen = false;
   createTasks:string;
-  constructor(public commonService: CommonService,private modalService:PrimeModalService) {
+  menus:any;
+  isFullSize = false;
+  element:any
+  constructor(@Inject(DOCUMENT) private document: any,public commonService: CommonService,private modalService:PrimeModalService) {
     this.logoImg = `${environment.APP_IMAGE_URL}assets/img/logo.png`;
     this.AllApps = `${environment.APP_IMAGE_URL}assets/img/allApps.png`;
     this.arrowImg = `${environment.APP_IMAGE_URL}assets/img/arrow.png`;
@@ -24,11 +28,39 @@ export class HeaderComponent implements OnInit {
     this.modalService.modal.subscribe(
       (modal) => (this.isModalOpen = modal)
     );
+    this.commonService.setMenu().subscribe((headers)=>{
+      this.menus = headers.HeaderMenu;
+    })
+    this.element = document.documentElement
   }
   createTask(){
     this.modalService.open()
     if(this.isModalOpen){
       this.createTasks = 'Task'
+    }
+  }
+  fullscreenToogle(){
+    this.isFullSize = !this.isFullSize;
+    if(this.isFullSize){
+      if(this.element.requestFullscreen){
+        this.element.requestFullscreen();
+      }else if(this.element.mozRequestFullScreen){
+        this.element.mozRequestFullScreen();
+      }else if(this.element.webkitRequestFullscreen){
+        this.element.webkitRequestFullscreen();
+      }else if(this.element.msRequestFullscreen){
+        this.element.msRequestFullscreen();
+      }
+    }else{
+      if(document.exitFullscreen){
+        this.document.exitFullscreen();
+      }else if(this.document.mozRequestFullScreen){
+        this.document.mozRequestFullScreen();
+      }else if(this.document.webkitRequestFullscreen){
+        this.document.webkitRequestFullscreen();
+      }else if(this.document.msExitFullscreen){
+        this.document.msExitFullscreen();
+      }
     }
   }
 }
--- jiraBoard/src/app/components/dashboard/service/common.service.ts
+++ jiraBoard/src/app/components/dashboard/service/common.service.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import Routes from 'src/app/Route';
 import { ModalService } from '../../common/_modal';
 
@@ -8,133 +10,42 @@ import { ModalService } from '../../common/_modal';
   providedIn: 'root',
 })
 export class CommonService {
-  menus: any;
   sidebarMenu: any;
   headers: any;
   profileMenu: any;
   DATA_ISSUES: any;
-  constructor(private modal: ModalService,private router:Router) {}
-  setMenu() {
-    const menu = [
-      {
-        id: '1',
-        menu_item: 'Your Work',
-        arrow: true,
-      },
-      {
-        id: '2',
-        menu_item: 'Projects',
-        arrow: true,
-      },
-      {
-        id: '3',
-        menu_item: 'Filters',
-        arrow: true,
-      },
-      {
-        id: '4',
-        menu_item: 'Dashboards',
-        arrow: true,
-      },
-      {
-        id: '5',
-        menu_item: 'People',
-        arrow: true,
-      },
-      {
-        id: '6',
-        menu_item: 'Apps',
-        arrow: true,
-      },
-    ];
-    return (this.menus = menu);
+  constructor(private modal: ModalService,private router:Router,private http:HttpClient) {}
+  public setMenu():Observable<any> {
+   return this.http.get("assets/header-menu.json");
   }
-  getProfileMenu() {
-    const profileMenu = [
-      {
-        items: [
-          {
-            label: 'Profile',
-            icon: 'pi pi-user',
-          },
-          {
-            label: 'Settings',
-            icon: 'pi pi-cog',
-          },
-          {
-            label: 'Logout',
-            icon: 'pi pi-sign-out',
-            command: () => {
-              this.logout();
-          }
-          },
-        ],
-      },
-    ];
-    return (this.profileMenu = profileMenu);
+  public getProfileMenu():Observable<any> {
+    // const profileMenu = [
+    //   {
+    //     items: [
+    //       {
+    //         label: 'Profile',
+    //         icon: 'pi pi-user',
+    //       },
+    //       {
+    //         label: 'Settings',
+    //         icon: 'pi pi-cog',
+    //       },
+    //       {
+    //         label: 'Logout',
+    //         icon: 'pi pi-sign-out',
+    //         command: () => {
+    //           this.logout();
+    //       }
+    //       },
+    //     ],
+    //   },
+    // ];
+    // return (this.profileMenu = profileMenu);
+    return this.http.get("assets/profile-menu.json");
   }
-  setSideBar() {
-    const sidebarMenus = [
-      {
-        id: '1',
-        menu_item: 'Planing',
-        childMenu: [
-          {
-            id: '2',
-            childMenu_item: 'Roadmap',
-            icon_class: 'bi bi-map',
-          },
-          {
-            id: '3',
-            childMenu_item: 'Backlog',
-            icon_class: 'bi bi-list-nested',
-          },
-          {
-            id: '4',
-            childMenu_item: 'Active sprints',
-            icon_class: 'bi bi-layout-three-columns',
-          },
-          {
-            id: '5',
-            childMenu_item: 'Reports',
-            icon_class: 'bi bi-graph-up-arrow',
-          },
-          {
-            id: '6',
-            childMenu_item: 'Issues',
-            icon_class: 'bi bi-view-list',
-            route_link:`${Routes.DASHBOARD.ISSUES}`
-          },
-          {
-            id: '7',
-            childMenu_item: 'Components',
-            icon_class: 'bi bi-collection',
-          },
-        ],
-      },
-      {
-        id: '8',
-        menu_item: 'Development',
-        childMenu: [
-          {
-            id: '9',
-            childMenu_item: 'Code',
-            icon_class: 'bi bi-code-slash',
-          },
-          {
-            id: '10',
-            childMenu_item: 'Rebase',
-            icon_class: 'bi bi-stack',
-          },
-        ],
-      },
-    ];
-    return (this.sidebarMenu = sidebarMenus);
+  public setSideBar():Observable<any> {
+    return this.http.get("assets/sidebar-menu.json")
   }
-  // createIssue(isModalOpen) {
-    // isModalOpen = true;
-    // this.modal.open('create-issue');
-  // }
   getIssues() {
     return (this.DATA_ISSUES = [
       { srno: '1', Task: 'coc-62225', devloper: 'Manav' },
